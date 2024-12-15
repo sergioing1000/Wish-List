c21af4c0d1a648a037ce84e98473ce82b932fcc3
Sergio Cruz <sergioing@yahoo.com>
2024-12-14 19:39:59 -0500

    modified:   src/Components/header.jsx

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Header.css";
+import "./header.css";
 
 import pencilSvg from "../assets/icons/pencil.svg";
 import worldsvg from "../assets/icons/world.svg";
